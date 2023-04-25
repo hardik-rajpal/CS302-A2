14301e6ce4ecb742face9ae72025464b7a0336cc
Hardik Rajpal <hardikraj08@gmail.com>
2023-04-25 17:42:03 +0530

    sapiens.pages check

--- tests/main.c
+++ tests/main.c
@@ -1,4 +1,3 @@
-int printf (const char *__restrict __fmt, ...);
 struct book
 {
     int pages;
@@ -16,7 +15,8 @@ int main()
 {
     int a, b, c, d;
     int f[10][10][10];
-    struct book *sapiens;
+    struct book sapiens;
+    sapiens.pages = 1;
     b = c && d;
     a = f[2][3][4]; /*f + 2*10*10*4 + 3*10*4 + 4*4*/
     a = b++;
